219bc98bdc7f5af4e5ec9a581c2dd06c3e75b21a
Roman-Kov <rojer_ko@bk.ru>
2020-03-02 02:25:48 +0300

    Задание 1 к уроку 3. Переделал на Rxjava

--- app/src/main/java/com/rojer_ko/homework/lesson_3/Lesson3MainActivity.java
+++ app/src/main/java/com/rojer_ko/homework/lesson_3/Lesson3MainActivity.java
@@ -3,25 +3,23 @@ package com.rojer_ko.homework.lesson_3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import com.rojer_ko.homework.R;
+import com.squareup.picasso.LruCache;
 import com.squareup.picasso.Picasso;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.SingleObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class Lesson3MainActivity extends AppCompatActivity {
 
@@ -33,17 +31,31 @@ public class Lesson3MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 10;
     private Uri uri;
+    private Disposable disposable;
+    private LruCache lruCache;
+    private Picasso picasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson3_main);
         ButterKnife.bind(this);
+        initPicasso();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
+    }
+
+    void initPicasso()
+    {
+        lruCache = new LruCache(getApplicationContext());
+        picasso = new Picasso.Builder(getApplicationContext()).memoryCache(lruCache).build();
     }
 
     @OnClick(R.id.choseJpgImageView)
     void getPicture(){
-
         if(checkPermissions()) {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("*/*");
@@ -61,30 +73,27 @@ public class Lesson3MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.convertBtn)
     void convert() {
-        if(uri!= null) {
-            Bitmap bitmap = null;
-            try {
-                InputStream inputStream = getContentResolver().openInputStream(uri);
-                bitmap = BitmapFactory.decodeStream(inputStream);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+        ConvertedImage convertedImage= new ConvertedImage(uri);
 
-            try {
-                    FileOutputStream fos = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath()+"/image.png");
-                if (bitmap != null) {
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-                }
-                fos.flush();
-                fos.close();
-                Uri pngImageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/image.png"));
-                Picasso.get().load(pngImageUri).into(pngImageView);
+        convertedImage.getUri(getApplicationContext())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<Uri>(){
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                    }
 
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                    Log.e("Error", e.toString());
-                }
-            }
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        lruCache.clear();
+                        picasso.load(uri).into(pngImageView);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                });
         }
 
     @Override
@@ -108,5 +117,4 @@ public class Lesson3MainActivity extends AppCompatActivity {
                 },
                 PERMISSION_REQUEST_CODE);
     }
-
 }
